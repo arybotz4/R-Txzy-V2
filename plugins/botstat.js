483c19e733aa575bd197db0714cef3189615a397
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 11:28:33 +0700

    Update botstat.js

--- plugins/botstat.js
+++ plugins/botstat.js
@@ -1,6 +1,5 @@
-let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-    let { anon, anticall, antispam, antitroli, backup, jadibot, groupOnly, nsfw } = global.db.data.settings[conn.user.jid]
+    let { anon, anticall, antispam, antitroli, backup, jadibot, groupOnly, nsfw, statusupdate, autogetmsg, antivirus, publicjoin } = global.db.data.settings[conn.user.jid]
     const chats = conn.chats.all()
     const groups = chats.filter(v => v.jid.endsWith('g.us'))
     let totaljadibot = [...new Set([...global.conns.filter(conn => conn.user && conn.state !== 'close').map(conn => conn.user)])]
@@ -8,29 +7,34 @@ let handler = async (m, { conn }) => {
     let _uptime = process.uptime() * 1000
     let uptime = clockString(_uptime)
 
-    let str = `
-╭─────[ *Status* ]────✧
-├◌ Aktif selama ${uptime}
-├◌ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
-├◌ *${groups.length}* Grup
-├◌ *${chats.length - groups.length}* Chat Pribadi
-├◌ *${Object.keys(global.db.data.users).length}* Pengguna
-├◌ *${totaljadibot.length}* Jadibot
-├◌ *${conn.blocklist.length}* Terblock
-├◌ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
-├◌ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
-╰────────────···
-╭─────[ *Pengaturan* ]────✧
-├ ${anon ? '✅' : '❌'} *Anon Chat*
-├ ${anticall ? '✅' : '❌'} *Anti Call*
-├ ${antispam ? '✅' : '❌'} *Anti Spam*
-├ ${antitroli ? '✅' : '❌'} *Anti Troli*
-├ ${backup ? '✅' : '❌'} *Auto Backup DB*
-├ ${groupOnly ? '✅' : '❌'} *Mode Grup*
-├ ${jadibot ? '✅' : '❌'} *Jadi Bot*
-├ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
-╰────────────···`.trim()
-     await conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), str, '©R-Txzy', 'Owner', '.owner', 'Menu', '.menu', m)
+    m.reply(`
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪STATUS ZIFABOTZ⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ Aktif selama ${uptime}
+║│➸ Baterai ${conn.battery != undefined ? `${conn.battery.value}% ${conn.battery.live ? '🔌 pengisian' : ''}` : 'tidak diketahui'}
+║│➸ *${groups.length}* Grup
+║│➸ *${chats.length - groups.length}* Chat Pribadi
+║│➸ *${Object.keys(global.db.data.users).length}* Pengguna
+║│➸ *${totaljadibot.length}* Jadibot
+║│➸ *${conn.blocklist.length}* Terblock
+║│➸ *${Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned).length}* Chat Terbanned
+║│➸ *${Object.entries(global.db.data.users).filter(user => user[1].banned).length}* Pengguna Terbanned
+╰─────────❉
+_____•••••••••
+
+╭═══════════════════════
+║╭──❉ 〔 ⳹ ❋ཻུ۪۪PERATURAN ZIFABOTZ⳹ ❋ཻུ۪۪ 〕 ❉────── 
+║│➸ ${anon ? '✅' : '❌'} *Anon Chat*
+║│➸ ${anticall ? '✅' : '❌'} *Anti Call*
+║│➸ ${antispam ? '✅' : '❌'} *Anti Spam*
+║│➸ ${antitroli ? '✅' : '❌'} *Anti Troli*
+║│➸ ${backup ? '✅' : '❌'} *Auto Backup DB*
+║│➸ ${groupOnly ? '✅' : '❌'} *Mode Grup*
+║│➸ ${jadibot ? '✅' : '❌'} *Jadi Bot*
+║│➸ ${nsfw ? '✅' : '❌'} *Mode Nsfw*
+╰─────────❉
+_____•••••••
+    `.trim())
 }
 handler.help = ['botstatus']
 handler.tags = ['info']
