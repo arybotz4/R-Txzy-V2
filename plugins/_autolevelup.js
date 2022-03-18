d920602225c1314039d2c2353e0a7955bf702dbb
AryBotz <99187011+arybotz4@users.noreply.github.com>
2022-03-18 11:39:17 +0700

    Update _autolevelup.js

--- plugins/_autolevelup.js
+++ plugins/_autolevelup.js
@@ -1,43 +1,94 @@
-let handler = m => m
-
+let { spawn } = require('child_process')
 let levelling = require('../lib/levelling')
-let fetch = require('node-fetch')
-handler.before = async function (m) {
-        let user = db.data.users[m.sender]
-        let users = Object.entries(db.data.users).map(([key, value]) => {
-                return { ...value, jid: key }
+module.exports = {
+  before(m) {
+    let user = global.DATABASE._data.users[m.sender]
+    if (!user.autolevelup) return !0
+    if (m.sender === conn.user.jid) return
+    let before = user.level * 1
+    while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+    if (before !== user.level) {
+      let d = new Date(new Date + 3600000)
+      let locale = 'id'
+      let time = d.toLocaleTimeString(locale, {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+      })
+      let name = this.getName(m.sender)
+      let lvlnow = user.level
+      let teks = `Selamat ${name} naik level` // teks di gambar tidak bisa ditambahkan emoticon
+      let str = `
+${teks} 
+
+• 🗿Level Sebelumnya : ${before}
+• 🗿Level Baru : ${lvlnow}
+• Pada Jam : ${time}
+
+*_Semakin sering berinteraksi dengan zifabotz Semakin Tinggi level kamu_*
+`.trim()
+      if (global.support.convert || global.support.magick || global.support.gm) {
+        let fontLevel = 'src/level_c.otf'
+        let fontTexts = 'src/texts.otf'
+        let xtsx = 'src/lvlup_template.jpg'
+        let bufs = []
+        let anotations = '+1385+260' // gapake else if kadang error
+        if (lvlnow > 2) anotations = '+1370+260'
+        if (lvlnow > 10) anotations = '+1330+260'
+        if (lvlnow > 50) anotations = '+1310+260'
+        if (lvlnow > 100) anotations = '+1260+260'
+       
+        const [_spawnprocess, ..._spawnargs] = [...(global.support.gm ? ['gm'] : global.support.magick ? ['magick'] : []),
+          'convert',
+          xtsx,
+          '-font',
+          fontTexts,
+          '-fill',
+          '#0F3E6A',
+          '-size',
+          '1024x784',
+          '-pointsize',
+          '68',
+          '-interline-spacing',
+          '-7.5',
+          '-annotate',
+          '+153+200',
+          teks,
+          //original together
+          '-font',
+          fontLevel,
+          '-fill',
+          '#0A2A48',
+          '-size',
+          '1024x784',
+          '-pointsize',
+          '140',
+          '-interline-spacing',
+          '-1.2',
+          '-annotate',
+          anotations,
+          lvlnow,
+          '-append',
+          'jpg:-'
+        ]
+        spawn(_spawnprocess, _spawnargs)
+          .on('error', e => {
+            throw e
+          })
+          .on('close', () => {
+            this.sendFile(m.chat, Buffer.concat(bufs), 'result.jpg', str, m)
+          })
+          .stdout.on('data', chunk => bufs.push(chunk))
+
+      } else {
+        m.reply(str, m.chat, {
+          contextInfo: {
+            mentionedJid: [m.sender]
+          }
         })
-        let who = m.sender
-        let discriminator = who.substring(9, 13)
-        let sortedLevel = users.map(toNumber('level')).sort(sort('level'))
-        let usersLevel = sortedLevel.map(enumGetKey)
-        let { min, xp, max } = levelling.xpRange(user.level, global.multiplier)
-        if (!user.autolevelup) return !0
-        let before = user.level * 1
-        while (levelling.canLevelUp(user.level, user.exp, global.multiplier)) user.level++
+      }
+    }
 
-        if (before !== user.level) {
-                let rank = await (await fetch(thumbfoto)).buffer()
-                        .then(async data => {
-                                await this.sendButtonLoc(m.chat, data, `@${who.split`@`[0]} _*Level Up!*_\n_${before}_ -> _${user.level}_`.trim(), watermark, 'Profile', ',my', m, { contextInfo: { mentionedJid: [who]}})
-                })
-        }
-        
-}
-module.exports = handler
-
-function sort(property, ascending = true) {
-        if (property) return (...args) => args[ascending & 1][property] - args[!ascending & 1][property]
-        else return (...args) => args[ascending & 1] - args[!ascending & 1]
-}
-
-function toNumber(property, _default = 0) {
-        if (property) return (a, i, b) => {
-                return { ...b[i], [property]: a[property] === undefined ? _default : a[property] }
-        }
-        else return a => a === undefined ? _default : a
-}
-
-function enumGetKey(a) {
-        return a.jid
+    return true
+  }
 }
